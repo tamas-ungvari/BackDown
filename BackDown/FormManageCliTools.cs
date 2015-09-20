023ed10b5044041542d8a5876efc4f37af05ae73
Peter Ungvari <ungvari.mail@gmail.com>
2015-09-20 12:26:01 +0200

    Read/write CliTools to/from file

--- BackDown/FormManageCliTools.cs
+++ BackDown/FormManageCliTools.cs
@@ -3,18 +3,52 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BackDown.Properties;
 
 namespace BackDown
 {
     public partial class FormManageCliTools : Form
     {
+
+        private DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<CliTool>));
+
         public FormManageCliTools()
         {
             InitializeComponent();
+            List<CliTool> cliTools = new List<CliTool>();
+            bindingSourceCliTools.DataSource = cliTools;
+            LoadCliToolsFromFile();
+        }
+
+        private void LoadCliToolsFromFile()
+        {
+            string fileName = Settings.Default.CLI_TOOLS_FILE;
+            if (!File.Exists(fileName))
+            {
+                return;
+            }
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                List<CliTool> cliTools = serializer.ReadObject(stream) as List<CliTool>;
+                bindingSourceCliTools.DataSource = cliTools;
+                dataGridViewCliTools.Refresh();
+            }
+        }
+
+        private void SaveCliToolsToFile()
+        {
+            string fileName = Settings.Default.CLI_TOOLS_FILE;
+            using (Stream stream = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.WriteObject(stream, bindingSourceCliTools.DataSource as List<CliTool>);
+            }
+
         }
 
         private void buttonNew_Click(object sender, EventArgs e)
@@ -29,6 +63,7 @@ namespace BackDown
             else
             {
                 dataGridViewCliTools.Refresh();
+                SaveCliToolsToFile();
             }
         }
 
@@ -42,12 +77,14 @@ namespace BackDown
             if (DialogResult.OK == cliToolForm.ShowDialog())
             {
                 dataGridViewCliTools.Refresh();
+                SaveCliToolsToFile();
             }
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
             bindingSourceCliTools.RemoveCurrent();
+            SaveCliToolsToFile();
         }
 
         private void dataGridViewCliTools_RowEnter(object sender, DataGridViewCellEventArgs e)
